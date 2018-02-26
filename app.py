d30c93e5645a27a32e77db73efbd67f50d3baca3
Pardha <pakoye@cisco.com>
2018-02-26 10:37:03 -0500

    host to lab

--- app.py
+++ app.py
@@ -130,6 +130,9 @@ def churn_ssh(host, db, lab):
         #s.sendline ('ls -l')
         #s.prompt()
         #print s.before
+        s.sendline('pwd')
+        s.prompt()
+        print s.before
         s.sendline ('./i_churn.py --lab ' +lab+ '--db-name '+db)
         s.prompt()
         print s.before
