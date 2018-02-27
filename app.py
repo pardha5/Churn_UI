ba267c25783e05c808b716d12b3afa81c6ac9c92
Pardha <pakoye@cisco.com>
2018-02-26 22:33:02 -0500

    Render Run Html Changes

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ def run_request():
     churn_ssh(host, db, lab)
 
     #return redirect(url_for('run'))
-    return redirect(url_for('/lab/run/result'))
+    return redirect(url_for('run'))
 
 @app.route("/lab/run/result", methods=["GET", "POST"])
 def run():
