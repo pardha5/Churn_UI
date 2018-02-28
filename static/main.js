18315003b005647671b280e6fe0daad569585987
Pardha <pakoye@cisco.com>
2018-02-28 18:20:42 -0500

    adv churn backend

--- static/main.js
+++ static/main.js
@@ -206,7 +206,7 @@ $(function() {
     console.log('in mdfy button click')
     $(".container_modify").width("800px")
     $('#left').animate({
-      'marginLeft' : "-=50px"
+      'marginLeft' : "-=5px"
     });
     $("#mdfy").show();
     $("#mdfy-btn").prop("disabled", true);
