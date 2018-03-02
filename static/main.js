3e3065e377d626df4d6cd0d9f7b98da0a59030f2
Pardha <pakoye@cisco.com>
2018-03-02 12:50:57 -0500

    ovr parse stuff for run.html

--- static/main.js
+++ static/main.js
@@ -186,9 +186,9 @@ $(function() {
                 p_ovr= JSON.parse(data['ovr']);
                 console.log(p_ovr);
                 ovr_s = JSON.stringify(p_ovr, undefined, 2);
-                ovr_j = ovr_s.replace(/<\s*\/?\s*br\s*.*?>/g, "<br>");
-                console.log(ovr_j)
-                window.location.href = '/run?lab=' + data['hlab'] + '&db=' + data['db']+ '&ovr=' + ovr_j+ '&cache=' + data['cache']+ '&m_t=' + data['m_t']+ '&log_lvl=' + data['log_lvl'];
+                console.log(ovr_s);
+                $("#ovr").html(ovr_s);
+                window.location.href = '/run?lab=' + data['hlab'] + '&db=' + data['db']+ '&ovr=' + ovr_s+ '&cache=' + data['cache']+ '&m_t=' + data['m_t']+ '&log_lvl=' + data['log_lvl'];
             },
       error: function(error){
                 console.log(error);
