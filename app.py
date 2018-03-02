c44ef12abadc7de72e81df47ef4f44317c211420
Pardha <pakoye@cisco.com>
2018-03-02 15:10:26 -0500

    sendline activated

--- app.py
+++ app.py
@@ -175,7 +175,7 @@ def churn_ssh(data):
                 print 'log level included'
                 print cmd
             #s.sendline ('./churn.py --lab ' +data['lab']+ ' --db-name '+data['db']+ ' --override \''+data['ovr']+'\'')
-        #s.sendline ('./churn.py --lab ' +lab+ ' --db-name '+db)
+        s.sendline (cmd)
         s.prompt()
         print s.before
         s.logout()
