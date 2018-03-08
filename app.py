392acd74a7e813e254906e2f072f6d83811ff2c3
Pardha <pakoye@cisco.com>
2018-03-07 19:01:39 -0500

    nohup check for bg run

--- app.py
+++ app.py
@@ -181,7 +181,7 @@ def churn_ssh(data):
         #s.sendline ('ps -ef|grep churn')
         #s.prompt()
         #print s.before
-        #s.sendline (cmd)
+        s.sendline (cmd)
         s.prompt()
         print s.before
         #s.sendline ('\n')
@@ -190,11 +190,11 @@ def churn_ssh(data):
         #s.sendline ('echo Results')
         #s.prompt()
         #print s.before
-        s.sendline('pwd')
+        s.sendline('sleep 5')
         s.prompt()
         print s.before
-        #s.sendline('./report.py -d '+data['db']+' -c churn_'+ str(i))
-        s.sendline('./report.py -d hot_1k_json -c churn_0')
+        s.sendline('./report.py -d '+data['db']+' -c churn_'+ str(i))
+        #s.sendline('./report.py -d hot_1k_json -c churn_0')
         s.prompt()
         report = s.before
         print '#########output#############'
